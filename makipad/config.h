993dc0ffd7d8dd782fabd2ee4bdec76cee38769b
Clito <119410746+Cli7o@users.noreply.github.com>
2024-12-18 21:02:28 +0100

    Add files via upload
    rotación de codificadores y manejo de capas

--- makipad/config.h
+++ makipad/config.h
@@ -1,4 +1,10 @@
 #define OLED_DISPLAY_128X64
+
+
 #define ENCODER_A_PINS { F7, F6 } //encoder1, encoder2
 #define ENCODER_B_PINS { F5, F4 }
+#define ENCODER_RESOLUTION 2
+
+
 #define I2C_DRIVER_REQUIRED
+
--- makipad/keymaps/default/keymap.c
+++ makipad/keymaps/default/keymap.c
@@ -1,46 +1,44 @@
 #include QMK_KEYBOARD_H
 #include "rtc.h"
 
+int              seleccion         = 0;
+static int       capa_seleccionada = 0; // numpad, asdf, blender
+static const int capa_minima       = 0;
+static const int capa_maxima       = 3;
+char             capa_str[16];
 
 
-#pragma region RTC
 void keyboard_post_init_user(void) {
     rtc_init();
 }
 void housekeeping_task_user(void) {
     rtc_task();
 }
-#pragma endregion
 
 
-#pragma region OLED
+
+
 bool oled_task_user() {
     oled_set_cursor(0, 0);
     oled_write(rtc_read_time_str(), false);
     oled_set_cursor(0, 1);
     oled_write(rtc_read_date_str(), false);
+    oled_set_cursor(0, 2);
+    snprintf(capa_str, sizeof(capa_str), "%d", capa_seleccionada);
+    oled_write(capa_str, false);
     return false;
 }
-#pragma endregion
 
 
-#pragma region ENCODERS
-/*
-void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { First encoder
-if (clockwise) {
-    tap_code(KC_PGDN);
-} else {
-    tap_code(KC_PGUP);
-}
-}
-}
-
-*/
-#pragma endregion
 
 
-#pragma region MATRIZ
+
+
+
+
+
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┐
@@ -51,16 +49,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ 1 │ 2 │ 3 │ - │
      * └───┴───┴───┴───┘
      */
+    
+    
     [0] = LAYOUT(
         KC_A,   KC_B,   KC_C,   KC_D,
         KC_E,   KC_F,   KC_G,   KC_H,
-        KC_I,   KC_J,   KC_K,   KC_L,
-        KC_M,   KC_N,   KC_NO,  KC_NO
+        KC_I,   KC_J,   KC_K,   LT(1, KC_L)
+    ),
+    [1] = LAYOUT(
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_TRNS
     )
 
+
 };
-#pragma endregion
 
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
+    if (get_highest_layer(layer_state) == 1) {
+
+        if (keycode == KC_F23 && record->event.pressed) {
+            capa_seleccionada++;
+        }
+        if (keycode == KC_F24 && record->event.pressed) {
+            capa_seleccionada--;
+        }
+
+        if (capa_seleccionada <= capa_minima) {
+            capa_seleccionada = capa_minima;
+        } else if (capa_seleccionada >= capa_maxima) {
+            capa_seleccionada = capa_maxima;
+        }
+    }
+    return true; // Permitir que otras acciones de la tecla se procesen
+}
+
+
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+
+    [0] = {ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [1] = {ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_F23, KC_F24)},
+
+};
 
 
